b39fba942ff5660a5740144f0da6665d527598d9
vagrant <vagrant@precise64.(none)>
2016-03-27 07:32:07 +0000

    corrected config.py encoding to ASCI

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-#SQLALCHEMY_DATABASE_URI = ‘mysql+pymysql://<db_user>:<db_password>@<endpoint>/<db_url>’
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<db_user>:<db_password>@<endpoint>/<db_url>'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dbroot:Blu3b3rryR0ck@aa1npu92ypyn8yt.cc6p0ojvcx3g.us-east-1.rds.amazonaws.com:3306/ebdb'
 
 # Uncomment the line below if you want to work with a local DB
